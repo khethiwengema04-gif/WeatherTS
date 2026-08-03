2b458d6dfb21c9cb2d5efa680255b540cc625fb7
khethiwe.ngema04 <khethiwe.ngema04@gmail.com>
2026-08-03 13:21:41 +0200

    created a card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import './App.css'
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import { Search } from './Components/Search/Search'
---
+++ src/Components/Search/Search.tsx
@@ -0,0 +1,11 @@
+import style from './Search.module.css'
+import { Searchbar } from './Searchbar'
+
+export const Search = () => {
+ return (
+     <div className={style['search-container']}>
+                 <Searchbar/>
+
+       </div>
+ )
+}
